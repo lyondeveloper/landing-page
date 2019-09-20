501c725a5e7b0575a4e8c3ac2273593c365223af
Jesus Rincon <contactojesuserincon@gmail.com>
2019-09-20 17:49:19 -0300

    Updates

---
+++ precache-manifest.caa04b56f6dd9249a8154f14c291ce5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0efbe911f7a341209c402fd2dd4b26c",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "0910861d54d450fe3700",
+    "url": "/landing-page/static/css/2.b62f5c36.chunk.css"
+  },
+  {
+    "revision": "35c88b8a52c6323e88ce",
+    "url": "/landing-page/static/css/main.76b45694.chunk.css"
+  },
+  {
+    "revision": "0910861d54d450fe3700",
+    "url": "/landing-page/static/js/2.272cf645.chunk.js"
+  },
+  {
+    "revision": "35c88b8a52c6323e88ce",
+    "url": "/landing-page/static/js/main.ddbb8956.chunk.js"
+  },
+  {
+    "revision": "036a5f9cfa818ced41fa",
+    "url": "/landing-page/static/js/runtime~main.a8192fd0.js"
+  },
+  {
+    "revision": "2f676ad24c1daae5e13d53a93966975d",
+    "url": "/landing-page/static/media/programming-2.2f676ad2.png"
+  },
+  {
+    "revision": "efb16061b176ca9388f387de30063c03",
+    "url": "/landing-page/static/media/programming-3.efb16061.jpeg"
+  }
+]);
